41e745e3c0363aafb3b340ce71ee7da9282baea5
gabrielbiscaia <biscadev@gmail.com>
2024-10-09 14:59:21 -0300

    style: changing loading color

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,7 @@
 export default function Loading() {
-    return (
-      <div className="flex justify-center items-center h-32">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="flex justify-center items-center h-32">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-red-500"></div>
+    </div>
+  );
+}

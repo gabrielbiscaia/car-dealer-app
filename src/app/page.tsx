c5cae63897d5ce03ec5cd5a92bdf8e1c531ad61f
gabrielbiscaia <biscadev@gmail.com>
2024-10-09 14:50:49 -0300

    refactor: putting background color in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-around p-24 bg-gray-900">
+    <main className="flex min-h-screen flex-col items-center justify-around p-24 ">
       <Header />
       <VehicleFilter />
     </main>

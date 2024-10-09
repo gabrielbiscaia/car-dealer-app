a683a44688568d9ff37b7b35975a36738c37b58f
gabrielbiscaia <biscadev@gmail.com>
2024-10-09 14:51:43 -0300

    style: bigger font and backgrund color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className="fixed flex top-0 w-full h-16 bg-transparent items-center border-b border-white">
-      <h1 className="text-2xl font-bold text-white p-8">Car Dealer App</h1>
+    <header className="fixed flex top-0 w-full h-16 bg-gray-900 items-center border-b border-white">
+      <h1 className="text-4xl font-bold text-white p-4">Car Dealer App</h1>
     </header>
   );
 }

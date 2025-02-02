7eb65a4651fe65a559baedd010960799abce84d1
gabrielbiscaia <gabrielbiscaia23@gmail.com>
2025-02-02 15:08:00 -0300

    fix: header name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="fixed flex top-0 w-full h-16 bg-gray-900 items-center border-b border-white">
-      <h1 className="text-4xl font-bold text-white p-4">Car Dealer App</h1>
+      <h1 className="text-4xl font-bold text-white p-4">Car Search App</h1>
     </header>
   );
 }
